e525f267fb3716343826a144547357b4f7811ac1
Aboubacar <elbacar08@gmail.com>
2019-03-13 15:39:39 +0100

    Role DAO without test

--- maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/dao/impl/RoleDao.java
+++ maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/dao/impl/RoleDao.java
@@ -5,6 +5,14 @@
  */
 package com.cours.ebenus.dao.impl;
 
+import com.cours.ebenus.dao.DriverManagerSingleton;
+import com.cours.ebenus.dao.IRoleDao;
+import com.cours.ebenus.dao.entities.Role;
+import com.cours.ebenus.utils.RoleUtils;
+import com.mysql.jdbc.Statement;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,195 +20,157 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import com.cours.ebenus.dao.DriverManagerSingleton;
-import com.cours.ebenus.dao.IRoleDao;
-import com.cours.ebenus.dao.entities.Role;
-import com.mysql.jdbc.Statement;
+import static com.cours.ebenus.utils.Constants.*;
+import static com.cours.ebenus.utils.RoleUtils.*;
 
 /**
- *
  * @author ElHadji
  */
 public class RoleDao /* extends AbstractDao<Role> */ implements IRoleDao {
 
-	private static final Log log = LogFactory.getLog(RoleDao.class);
-	Connection conn = null;
-	PreparedStatement statement = null;
-	ResultSet result = null;
+    private static final Log log = LogFactory.getLog(RoleDao.class);
+    Connection conn = null;
+    PreparedStatement statement = null;
+    ResultSet result = null;
 
-	public RoleDao() {
-		// super(Role.class);
-		try {
-			conn = DriverManagerSingleton.getConnectionInstance().getConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public RoleDao() {
+        // super(Role.class);
+        try {
+            conn = DriverManagerSingleton.getConnectionInstance().getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public List<Role> findAllRoles() {
-		List<Role> roles = new ArrayList<Role>();
+    @Override
+    public List<Role> findAllRoles() {
+        return getRoleByCriteria(null);
+    }
 
-		try {
+    @Override
+    public Role findRoleById(int idRole) {
+        return getRoleByCriteria(idRole).get(0);
+    }
 
-			statement = conn.prepareStatement("SELECT * FROM Role");
-			// SQL request for table user
-			ResultSet rs = statement.executeQuery();
-			int idRole;
-			int version;
-			String identifiant;
-			String description;
+    @Override
+    public List<Role> findRoleByIdentifiant(String identifiantRole) {
+        return getRoleByCriteria(identifiantRole);
+    }
 
-			while (rs.next()) {
-				idRole = rs.getInt("idRole");
-				identifiant = rs.getString("identifiant");
-				version = rs.getInt("version");
-				description = rs.getString("description");
-				Role role = new Role(idRole, identifiant, description, version);
+    @Override
+    public Role createRole(Role role) {
 
-				roles.add(role);
-			}
+        if (role != null) {
+            try {
+                statement = conn.prepareStatement(createRoleQuery + qote + role.getIdentifiant()
+                                + comaString + role.getDescription() + qote + coma + role.getVersion() + ")",
+                        Statement.RETURN_GENERATED_KEYS);
 
-		} catch (SQLException se) {
-			// Handle errors for JDBC
-			se.printStackTrace();
-		} catch (Exception e) {
-			// Handle errors for Class.forName
-			e.printStackTrace();
-		}
+                statement.executeUpdate();
+                ResultSet rs = statement.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    role.setIdRole(id);
+                }
 
-		return roles;
-	}
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-	@Override
-	public Role findRoleById(int idRole) {
-		Role role = null;
-		try {
+        return role;
+    }
 
-			statement = conn.prepareStatement("SELECT * FROM Role WHERE idRole = " + idRole);
+    @Override
+    public Role updateRole(Role role) {
+        try {
 
-			ResultSet rs = statement.executeQuery();
-			int version;
-			String identifiant;
-			String description;
+            statement = conn.prepareStatement(updateRoleQuery + qote + role.getIdentifiant() + qote
+                    + coma + qote + RoleLib.DESCRIPTION + qote + equal + qote + role.getDescription() + qote + " WHERE "
+                    + RoleLib.ID.getField() + equal + role.getIdRole());
 
-			if (rs.next()) {
-				idRole = rs.getInt("idRole");
-				identifiant = rs.getString("identifiant");
-				version = rs.getInt("version");
-				description = rs.getString("description");
-				role = new Role(idRole, identifiant, description, version);
-			}
+            statement.executeUpdate();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		} catch (SQLException se) {
-			// Handle errors for JDBC
-			se.printStackTrace();
-		} catch (Exception e) {
-			// Handle errors for Class.forName
-			e.printStackTrace();
-		}
+        return role;
+    }
 
-		return role;
-	}
+    @Override
+    public boolean deleteRole(Role role) {
+        try {
 
-	@Override
-	public List<Role> findRoleByIdentifiant(String identifiantRole) {
-		List<Role> roles = new ArrayList<Role>();
+            statement = conn.prepareStatement(deleteRoleQuery + role.getIdRole());
+            int rs = statement.executeUpdate();
+            return (rs >= 1);
 
-		try {
-			statement = conn.prepareStatement("SELECT * FROM Role WHERE identifiant = '" + identifiantRole + "'");
-			ResultSet rs = statement.executeQuery();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-			int idRole;
-			int version;
-			String identifiant;
-			String description;
+    private ArrayList<Role> getRoles(ResultSet rs) {
+        ArrayList<Role> roles = new ArrayList<>();
+        try {
 
-			while (rs.next()) {
-				idRole = rs.getInt("idRole");
-				identifiant = rs.getString("identifiant");
-				version = rs.getInt("version");
-				description = rs.getString("description");
-				Role role = new Role(idRole, identifiant, description, version);
+            int idRole;
+            int version;
+            String identifiant;
+            String description;
 
-				roles.add(role);
-			}
+            while (rs.next()) {
+                idRole = rs.getInt(RoleUtils.RoleLib.ID.getField());
+                identifiant = rs.getString(RoleUtils.RoleLib.IDENTIFIANT.getField());
+                version = rs.getInt(RoleUtils.RoleLib.VERSION.getField());
+                description = rs.getString(RoleUtils.RoleLib.DESCRIPTION.getField());
+                Role role = new Role(idRole, identifiant, description, version);
 
-		} catch (SQLException se) {
-			// Handle errors for JDBC
-			se.printStackTrace();
-		} catch (Exception e) {
-			// Handle errors for Class.forName
-			e.printStackTrace();
-		}
+                roles.add(role);
+            }
 
-		return roles;
-	}
+        } catch (SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Handle errors for Class.forName
+            e.printStackTrace();
+        }
+        return roles;
+    }
 
-	@Override
-	public Role createRole(Role role) {
+    private List<Role> getRoleByCriteria(Object value) {
+        List<Role> roles = null;
+        try {
+            if (value != null) {
+                if (value instanceof Integer) {
+                    statement = conn.prepareStatement(getRoleByIdQuery + value);
+                } else if (value instanceof String) {
+                    statement = conn.prepareStatement(getRoleByIdentifantQuery + qote + value + qote);
+                }
+            } else {
+                statement = conn.prepareStatement(getAllRoleQuery);
+            }
+            ResultSet rs = statement.executeQuery();
+            if (rs != null)
+                roles = new ArrayList<>(getRoles(rs));
+        } catch (SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Handle errors for Class.forName
+            e.printStackTrace();
+        }
 
-		try {
+        return roles;
+    }
 
-			System.out.print("INSERT INTO `Role` (identifiant, description, version) VALUES ('" + role.getIdentifiant()
-					+ "', '" + role.getDescription() + "', " + role.getVersion() + ")");
 
-			statement = conn
-					.prepareStatement(
-							"INSERT INTO `Role` (identifiant, description, version) VALUES ('" + role.getIdentifiant()
-									+ "', '" + role.getDescription() + "', " + role.getVersion() + ")",
-							Statement.RETURN_GENERATED_KEYS);
-
-			statement.executeUpdate();
-			ResultSet rs = statement.getGeneratedKeys();
-			if (rs.next()) {
-				int id = rs.getInt(1);
-				System.out.println("Inserted ID " + id); // display inserted record
-				role.setIdRole(id);
-			}
-
-		} catch (SQLException se) {
-			se.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return role;
-	}
-
-	@Override
-	public Role updateRole(Role role) {
-		try {
-
-			statement = conn.prepareStatement("UPDATE `Role` SET `identifiant`='" + role.getIdentifiant()
-					+ "', `description`='" + role.getDescription() + "' WHERE idRole=" + role.getIdRole());
-
-			statement.executeUpdate();
-		} catch (SQLException se) {
-			se.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return role;
-	}
-
-	@Override
-	public boolean deleteRole(Role role) {
-		try {
-
-			statement = conn.prepareStatement("DELETE FROM `Role` WHERE `idRole`=" + role.getIdRole());
-			int rs = statement.executeUpdate();
-			return (rs >= 1);// return boolean of comparaison
-
-		} catch (SQLException se) {
-			se.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
 }
--- maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/utils/RoleUtils.java
+++ maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/utils/RoleUtils.java
@@ -4,6 +4,10 @@ public class RoleUtils {
 
     public static final String getRoleByIdQuery = "SELECT * FROM Role WHERE idRole =";
     public static final String getRoleByIdentifantQuery = "SELECT * FROM Role WHERE identifiant =";
+    public static final String getAllRoleQuery = "SELECT * FROM Role";
+    public static final String createRoleQuery = "INSERT INTO `Role` (identifiant, description, version) VALUES (";
+    public static final String updateRoleQuery = "UPDATE `Role` SET `identifiant`=";
+    public static final String deleteRoleQuery = "DELETE FROM `Role` WHERE `idRole`=";
 
 
     public enum RoleLib {
