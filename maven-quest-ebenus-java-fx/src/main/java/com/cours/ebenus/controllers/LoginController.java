6f9d57b641f4f76d11bf055ce7f82d198223e416
Aboubacar <elbacar08@gmail.com>
2019-03-18 14:50:25 +0100

    work in progress for

--- maven-quest-ebenus-java-fx/src/main/java/com/cours/ebenus/controllers/LoginController.java
+++ maven-quest-ebenus-java-fx/src/main/java/com/cours/ebenus/controllers/LoginController.java
@@ -6,8 +6,16 @@
 package com.cours.ebenus.controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import com.cours.ebenus.dao.entities.Utilisateur;
+import com.cours.ebenus.dao.impl.UtilisateurDao;
+import com.cours.ebenus.factory.AbstractDaoFactory;
+import com.cours.ebenus.service.IServiceFacade;
+import com.cours.ebenus.service.ServiceFacade;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -28,40 +36,56 @@ import javafx.stage.Stage;
  */
 public class LoginController implements Initializable {
 
-	private static final Log logger = LogFactory.getLog(LoginController.class);
+    private static final Log logger = LogFactory.getLog(LoginController.class);
 
-	@FXML
-	private TextField identifiant;
+    @FXML
+    private TextField identifiant;
+    @FXML
+    private PasswordField motPasse;
 
-	private PasswordField motPasse;
+    private List<Utilisateur> usersToLoadFromDb;
 
-	// private IServiceFacade serviceFacade = null;
-	/**
-	 * Initializes the controller class.
-	 *
-	 * @param url
-	 * @param rb
-	 */
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
+    // private IServiceFacade serviceFacade = null;
 
-		/*
-		 * IServiceFacade service = null; System.out.println("second");
-		 * service.getUtilisateurDao().findAllUtilisateurs();
-		 */
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
 
-	}
+        IServiceFacade service = new ServiceFacade(AbstractDaoFactory.FactoryDaoType.JDBC_DAO_FACTORY);
+        System.out.println("second");
+        if(service.getUtilisateurDao() != null){
+            usersToLoadFromDb = new ArrayList<>(service.getUtilisateurDao().findAllUtilisateurs());
+        }
+    }
 
-	@FXML
-	public void authenticate(ActionEvent event) throws Exception {
-		if ("admin".equals(identifiant.getText()) && "admin".equals(identifiant.getText())) {
-
-			Stage stage = new Stage();
-			Parent root = FXMLLoader.load(getClass().getResource("/views/home.fxml"));
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
-
-		}
-	}
+    @FXML
+    public void authenticate(ActionEvent event) throws Exception {
+        String identifyText = this.identifiant.getText();
+        String passwordText = this.motPasse.getText();
+        if (!identifyText.isEmpty() && !passwordText.isEmpty()) {
+            if(isExist(identifyText, passwordText)) {
+                Stage stage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("/views/home.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+        }else {
+            System.err.println("fields empty");
+        }
+    }
+    private boolean isExist(String identify, String password){
+        boolean isFound = false;
+        Iterator<Utilisateur> it = usersToLoadFromDb.iterator();
+        while(!isFound && it.hasNext()){
+            Utilisateur currentUser = it.next();
+            isFound = identify.equals(currentUser.getIdentifiant()) && password.equals(currentUser.getMotPasse());
+        }
+        return isFound;
+    }
 }
