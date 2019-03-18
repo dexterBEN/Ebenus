534be5e18d49e9f8fd51b45dcc488a7918496908
Aboubacar <elbacar08@gmail.com>
2019-03-18 17:58:48 +0100

    wip home

--- maven-quest-ebenus-java-fx/src/main/java/com/cours/ebenus/ihm/utils/LibUtils.java
+++ maven-quest-ebenus-java-fx/src/main/java/com/cours/ebenus/ihm/utils/LibUtils.java
@@ -1,14 +1,20 @@
 package com.cours.ebenus.ihm.utils;
 
 import com.cours.ebenus.dao.entities.Utilisateur;
+import com.cours.ebenus.models.UserModel;
 
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
 public class LibUtils {
 
-    public static void dialogMessage(String message){
+    private static String EMPTY = "";
+
+    public static void dialogMessage(String message) {
         JOptionPane.showMessageDialog(null, message);
     }
 
@@ -20,5 +26,30 @@ public class LibUtils {
             isFound = identify.equals(currentUser.getIdentifiant()) && password.equals(currentUser.getMotPasse());
         }
         return isFound;
+    }
+
+    public static List<UserModel> setUserToUserModel(List<Utilisateur> users) {
+        List<UserModel> userModelList = new ArrayList<>();
+        if (!users.isEmpty()) {
+            for (Utilisateur user : users) {
+                UserModel userModel = new UserModel();
+                userModel.setCivilite(user.getCivilite());
+                userModel.setDateCreation(getDate(user.getDateCreation()));
+                userModel.setRole(user.getRole() != null ? user.getRole().getIdentifiant() : EMPTY);
+                userModel.setPrenom(user.getPrenom());
+                userModel.setNom(user.getNom());
+            }
+        }
+        return userModelList;
+    }
+
+    public static String getDate(Date date) {
+        if (date != null) {
+            String pattern = "dd/MM/aaaa";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            return simpleDateFormat.format(date);
+        }else{
+            return EMPTY;
+        }
     }
 }
