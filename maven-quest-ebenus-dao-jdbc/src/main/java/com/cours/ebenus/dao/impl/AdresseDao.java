1582ee4c3e7fbca7e59f33bc8d46871fc28faa92
Aboubacar KOUMARE <aboubacar.koumare@niji.fr>
2019-04-03 06:30:50 +0200

    WIP commande and refacto

--- maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/dao/impl/AdresseDao.java
+++ maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/dao/impl/AdresseDao.java
@@ -1,11 +1,27 @@
 package com.cours.ebenus.dao.impl;
 
+import com.cours.ebenus.dao.DriverManagerSingleton;
 import com.cours.ebenus.dao.IDao;
 import com.cours.ebenus.dao.entities.Adresse;
+import com.cours.ebenus.dao.entities.ArticleCommande;
 
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.cours.ebenus.utils.DaoUtils.genericQuery;
+
 public class AdresseDao implements IDao<Adresse> {
+
+    Connection connection = null;
+
+    public AdresseDao() {
+        try {
+            connection = DriverManagerSingleton.getConnectionInstance().getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public List<Adresse> findAll() {
         return null;
@@ -34,5 +50,16 @@ public class AdresseDao implements IDao<Adresse> {
     @Override
     public boolean delete(Adresse adresse) {
         return false;
+    }
+
+    @Override
+    public List<Adresse> sendQuery(String query, Adresse adresse) {
+        List<Adresse> adresses = new ArrayList<>();
+        for (Object object : genericQuery(query, adresse, connection)) {
+            if (object instanceof Adresse) {
+                adresses.add((Adresse) object);
+            }
+        }
+        return adresses;
     }
 }
--- maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/dao/impl/ArticleCommandeDao.java
+++ maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/dao/impl/ArticleCommandeDao.java
@@ -1,11 +1,27 @@
 package com.cours.ebenus.dao.impl;
 
+import com.cours.ebenus.dao.DriverManagerSingleton;
 import com.cours.ebenus.dao.IDao;
 import com.cours.ebenus.dao.entities.ArticleCommande;
 
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.cours.ebenus.utils.DaoUtils.genericQuery;
+
 public class ArticleCommandeDao implements IDao<ArticleCommande> {
+
+
+    Connection connection = null;
+
+    public ArticleCommandeDao() {
+        try {
+            connection = DriverManagerSingleton.getConnectionInstance().getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public List<ArticleCommande> findAll() {
@@ -35,5 +51,16 @@ public class ArticleCommandeDao implements IDao<ArticleCommande> {
     @Override
     public boolean delete(ArticleCommande articleCommande) {
         return false;
+    }
+
+    @Override
+    public List<ArticleCommande> sendQuery(String query, ArticleCommande articleCommande) {
+        List<ArticleCommande> articleCommandes = new ArrayList<>();
+        for (Object object : genericQuery(query, articleCommande, connection)) {
+            if (object instanceof ArticleCommande) {
+                articleCommandes.add((ArticleCommande) object);
+            }
+        }
+        return articleCommandes;
     }
 }
