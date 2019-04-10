234ea0ac79a36fecf19d34e836d031ee7da34264
Aboubacar <elbacar08@gmail.com>
2019-04-10 22:33:43 +0200

    WIP

--- maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/factory/DaoFactory.java
+++ maven-quest-ebenus-dao-jdbc/src/main/java/com/cours/ebenus/factory/DaoFactory.java
@@ -12,6 +12,10 @@ import com.cours.ebenus.dao.entities.Adresse;
 import com.cours.ebenus.dao.entities.ArticleCommande;
 import com.cours.ebenus.dao.entities.Commande;
 import com.cours.ebenus.dao.entities.Product;
+import com.cours.ebenus.dao.impl.AdresseDao;
+import com.cours.ebenus.dao.impl.ArticleCommandeDao;
+import com.cours.ebenus.dao.impl.CommandeDao;
+import com.cours.ebenus.dao.impl.ProductDao;
 import com.cours.ebenus.dao.impl.RoleDao;
 import com.cours.ebenus.dao.impl.UtilisateurDao;
 
@@ -34,24 +38,24 @@ public class DaoFactory extends AbstractDaoFactory {
 	@Override
 	public IDao<Product> getProductDao() {
 		// TODO Auto-generated method stub
-		return null;
+		return new ProductDao();
 	}
 
 	@Override
 	public IDao<Adresse> getAdresseDao() {
 		// TODO Auto-generated method stub
-		return null;
+		return new AdresseDao();
 	}
 
 	@Override
 	public IDao<Commande> getCommandeDao() {
 		// TODO Auto-generated method stub
-		return null;
+		return new CommandeDao();
 	}
 
 	@Override
 	public IDao<ArticleCommande> getArticleCOmmande() {
 		// TODO Auto-generated method stub
-		return null;
+		return new ArticleCommandeDao();
 	}
 }
